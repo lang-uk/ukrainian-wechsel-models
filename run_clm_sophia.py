9bd538ad508f5f00f91d256cfb000fa437541f55
Dmitry Chaplinsky <dchaplinsky@conversionscience.co.uk>
2023-06-08 00:54:22 +0300

    Making it work with custom transformers like mpt7b

--- run_clm_sophia.py
+++ run_clm_sophia.py
@@ -311,6 +311,7 @@ def main():
             config=config,
             cache_dir=model_args.cache_dir,
             revision=model_args.model_revision,
+            trust_remote_code=True,
             use_auth_token=True if model_args.use_auth_token else None,
         )
     else:
